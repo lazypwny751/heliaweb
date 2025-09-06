913bb0aa812afdbd4d4f4dae25a4a35e9d66cdeb
lazypwny751 <testfordnshijacking@gmail.com>
2025-09-06 23:50:59 +0300

    add pages

---
+++ frontend/src/template/admin/Admin.tsx
@@ -0,0 +1,7 @@
+import admin from "./index.html";
+
+export function Admin() {
+  return <>{admin}</>;
+}
+
+export default Admin;
---
+++ frontend/src/template/user/User.tsx
@@ -0,0 +1,7 @@
+import user from "./index.html";
+
+export function User() {
+  return <>{user}</>;
+}
+
+export default User;
