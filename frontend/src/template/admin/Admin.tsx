d608bf42acbfc95e9862fc3f991e3acb1c994e4c
lazypwny751 <testfordnshijacking@gmail.com>
2025-10-27 02:11:20 +0300

    update base rust to v

--- frontend/src/template/admin/Admin.tsx
+++
@@ -1,7 +0,0 @@
-import admin from "./index.html";
-
-export function Admin() {
-  return <>{admin}</>;
-}
-
-export default Admin;
--- frontend/src/template/user/User.tsx
+++
@@ -1,7 +0,0 @@
-import user from "./index.html";
-
-export function User() {
-  return <>{user}</>;
-}
-
-export default User;
